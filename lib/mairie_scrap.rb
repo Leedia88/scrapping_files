83e469a8e2a42d576deaa61af70919d11e341fef
Lydia Lachenaud <lydia.lachenaud@gmail.com>
2022-04-21 15:03:42 +0200

    correction mairie

--- lib/mairie_scrap.rb
+++ lib/mairie_scrap.rb
@@ -42,11 +42,16 @@ def get_townhall_email(townhall_url)
 end
 
 def get_townhall_urls(townhall_url)
+    array = []
     email_liste =[]
     Nokogiri::HTML(URI.open(townhall_url)).xpath("//a[@class='lientxt']/@href").each do |link|
-        email_liste << get_townhall_email("http://annuaire-des-mairies.com#{link.text[1..-1]}")
+        # email_liste << get_townhall_email("http://annuaire-des-mairies.com#{link.text[1..-1]}")
+        result = Hash.new
+        result[link.text[5..-6].capitalize] = get_townhall_email("http://annuaire-des-mairies.com#{link.text[1..-1]}")
+        array << result
     end
-    return email_liste
+
+    return array
     #pour chaque ville, on construit l'email/ on applique la fonction get et on ajoute l'email à la liste
 end
 
